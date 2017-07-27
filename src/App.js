8c3d19d3b11502210d0d9e7e14b82633f357d062
Tan Nguyen <tannd1993@gmail.com>
2017-07-27 23:16:12 +0700

    demo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,78 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      people: [],
+      next: '',
+      previous: '',
+      error: false,
+    };
+  }
+
+  callAjax = (url) => {
+    $.ajax({
+      url,
+      beforeSend: () => {
+        if (this.loading) {
+          return false;
+        }
+
+        this.loading = true;
+      },
+      success: ({ results, previous, next }) => {
+        console.log(previous, next);
+        this.setState({
+          people: results,
+          next,
+          previous,
+          error: false,
+        });
+      },
+      error: (xhr, status, error) => {
+        this.setState({
+          error: true,
+        });
+      },
+      complete: () => {
+        this.loading = false;
+      }
+    });
+  }
+
+  componentWillMount() {
+    this.callAjax('https://swapi.co/api/people/');
+  }
+
+  renderPeople = () => {
+    return this.state.people.map((person, index) => {
+      return (<li key={index}>{person.name}</li>);
+    });
+  }
+
+  handlePrev = () => {
+    if (!this.state.previous) {
+      return;
+    }
+
+    this.callAjax(this.state.previous);
+  }
+
+  handleNext = () => {
+    if (!this.state.next) {
+      return;
+    }
+
+    this.callAjax(this.state.next);
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +83,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <ul>
+          {this.renderPeople()}
+        </ul>
+        {this.state.error && (<p>Please fetch again</p>)}
+        <button onClick={this.handlePrev} disabled={!this.state.previous}>Previous</button>
+        <button onClick={this.handleNext} disabled={!this.state.next}>Next</button>
       </div>
     );
   }
